7ab816a8a681e890698d5a7e16d87f00821fdf68
Ashwani <ashwani90125@gmail.com>
2025-10-05 14:48:09 +0530

    Add settings page

---
+++ app/settings/_layout.js
@@ -0,0 +1,29 @@
+// app/home/_layout.js
+import { Stack, useRouter } from 'expo-router';
+import { LeftHeader } from '../../components/LeftHeader';
+import { LoginButton } from '../../components/LoginButton';
+
+export default function HomeStackLayout() {
+  const router = useRouter();
+
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={({ navigation }) => ({
+          title: ' Settings',
+
+          // LEFT: hamburger that opens parent drawer
+          headerLeft: () => (
+            <LeftHeader navigation={navigation}/>
+          ),
+
+          // RIGHT: login button (same as before)
+          headerRight: () => (
+            <LoginButton/>
+          ),
+        })}
+      />
+    </Stack>
+  );
+}
